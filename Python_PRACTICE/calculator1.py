ed20e85650093ec8f6eb7b16379eb7522081ec87
Sujan <rsujan140.in@gmail.com>
2026-08-24 11:08:27 +0545

    Rename Python_PROJECT folder to Python_PRACTICE

---
+++ Python_PRACTICE/calculator1.py
@@ -0,0 +1,21 @@
+first_num=8
+second_num=6
+
+
+addition = first_num + second_num
+subtraction = first_num - second_num
+multiplication = first_num * second_num
+modules = first_num % second_num
+division = first_num / second_num
+floor_division = first_num // second_num
+power = first_num ** second_num
+average = (first_num+second_num) / 2
+
+print(" 1. Addition of first and second number is: ", addition)
+print(" 2. Subtraction of first and second number is: ", subtraction)
+print(" 3. Multiplication of first and second number is: ", multiplication)
+print(" 5. Modules of first and second number is: ", division)
+print(" 4. Division of first and second number is: ", division)
+print(" 6. Floor Division or Integer division of first and second number is: ", floor_division)
+print(" 7. Power of first to second number is: ", power)
+print(" 8. Average of first and second number is: ", average)
